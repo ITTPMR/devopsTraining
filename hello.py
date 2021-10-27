5bb7971f28c3911026c7c818617b9e6090869cf1
ITTPMR <92515132+ITTPMR@users.noreply.github.com>
2021-10-27 15:04:17 +0530

    Add simple python file

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("+++++++++++++++++++++++++++++++")
+print("Welcome to the Devops training") 
+print("+++++++++++++++++++++++++++++++")
+print("In this training, we learn AWS using Terraform, Jenkins, Docker, Ansible")
