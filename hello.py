9021e12e734cc0055fd955db4ba2cf2a1c06cadf
ITTPMR <92515132+ITTPMR@users.noreply.github.com>
2021-11-09 13:39:28 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("+++++++++++++++++++++++++++++++")
-print("Welcome to the Devops training") 
+print("Welcome to the Azure/AWS Devops training") 
 print("+++++++++++++++++++++++++++++++")
 print("In this training, we learn AWS using Terraform, Jenkins, Docker, Ansible")
